45e9fad26067fd6ceb9dafdb5ac54db39daadb79
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-21 17:27:41 -0700

    Update Query.php

--- src/mcpe/Query.php
+++ src/mcpe/Query.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace mcpe;
 
-class Query{
+class Query {
+
     /** @var string */
     protected $ip;
     /** @var int */
@@ -15,16 +16,16 @@ class Query{
     /** @var resource */
     protected $socket = null;
 
-    public function __construct(string $ip, int $port, int $timeout = 1){
+    public function __construct($ip, $port, $timeout = 1) {
         $this->ip = $ip;
         $this->port = $port;
         $this->timeout = $timeout;
     }
 
-    public function connect() : void{
+    public function connect() : void {
         $this->resolveSRV();
         $socket = stream_socket_client("udp://" . $this->ip . ":" . $this->port, $errno, $errstr, $this->timeout);
-        if($errno || $socket === false){
+        if ($errno || $socket === false) {
             throw new \RuntimeException("Socket connection error");
         }
 
@@ -34,11 +35,11 @@ class Query{
         stream_set_blocking($this->socket, true);
     }
 
-    public function close() : void{
+    public function close() : void {
         fclose($this->socket);
     }
 
-    public function getInfo() : \stdClass{
+    public function getInfo() : \stdClass {
         $data = new \stdClass;
 
         $data->query = $this->getQueryInfo();
@@ -47,10 +48,10 @@ class Query{
         return $data;
     }
 
-    public function getQueryInfo() : ?\stdClass{
+    public function getQueryInfo() : ?\stdClass {
         $packet = $this->writePacket(9);
 
-        if($packet === null){
+        if ($packet === null) {
             throw new \RuntimeException("Challenge packet error");
         }
 
@@ -58,7 +59,7 @@ class Query{
 
         $query = new \stdClass;
 
-        if($packet === null){
+        if ($packet === null) {
             $query->error = "Statistic packet error";
             return $query;
         }
@@ -66,7 +67,7 @@ class Query{
         $data = substr($packet["payload"], 11); // splitnum + 2 int
         $data = explode("\x00\x00\x01player_\x00\x00", $data);
 
-        if(count($data) !== 2){
+        if (count($data) !== 2) {
             $query->error = "Data error";
             return $query;
         }
@@ -74,47 +75,47 @@ class Query{
         $query->players = explode("\x00", substr($data[1], 0, -2));
         $data = explode("\x00", $data[0]);
         $last = false;
-        foreach($data as $val){
-            if($last === false){
+        foreach ($data as $val) {
+            if ($last === false) {
                 $last = $val;
-            }else{
+            } else {
                 $query->{$last} = $val;
                 $last = false;
             }
         }
 
         // Parse "plugins", if any
-        if($query->plugins !== ""){
+        if ($query->plugins !== "") {
             $plugins = explode(": ", $query->plugins, 2);
 
             $query->raw_plugins = $plugins;
             $query->software = $plugins[0];
 
-            if(count($plugins) === 2){
+            if (count($plugins) === 2) {
                 $query->plugins = explode("; ", $plugins[1]);
             }
-        }else{
+        } else {
             $query->software = "Vanilla";
         }
 
         return $query;
     }
 
-    public function getPingInfo() : ?\stdClass{
+    public function getPingInfo() : ?\stdClass {
         $pingPacket = "\x01" . "\x00\x00\x00\x00\x00\x00\x00\x00" . "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
         //packetId + long(0) + magic
 
         fwrite($this->socket, $pingPacket);
         $packet = fread($this->socket, 65535);
 
-        if($packet === false){
+        if ($packet === false) {
             return null;
         }
 
         $result = new \stdClass;
         $data = explode(";", substr($packet, 1 + 8 + 8 + 16 + 2));
-        foreach($data as $offset => $value){
-            switch($offset){
+        foreach ($data as $offset => $value) {
+            switch ($offset) {
                 case 0:
                     $result->game_id = $value;
                     break;
@@ -148,19 +149,19 @@ class Query{
         return $result;
     }
 
-    public function resolveSRV() : void{
+    public function resolveSRV() : void {
         $address = &$this->ip;
 
-        if(ip2long($address) !== false){
+        if (ip2long($address) !== false) {
             return;
         }
 
         $record = dns_get_record("_minecraft._tcp.$address", DNS_SRV);
-        if($record === false || count($record) === 0){
+        if ($record === false || count($record) === 0) {
             return;
         }
 
-        if(isset($record[0]["target"])){
+        if (isset($record[0]["target"])) {
             $address = $record[0]["target"];
         }
     }
@@ -169,13 +170,13 @@ class Query{
      * @return int|string[]
      * @phpstan-return array{packetType: int, sessionID: int, payload: string}
      */
-    protected function writePacket(int $command, string $append = "") : ?array{
+    protected function writePacket(int $command, string $append = "") : ?array {
         $command = "\xFE\xFD" . chr($command) . pack("c*", 0x01, 0x02, 0x03, 0x04) . $append;
 
         fwrite($this->socket, $command);
 
         $buffer = fread($this->socket, 65535);
-        if($buffer === false || strlen($buffer) < 5 || $buffer[0] !== $command[2]){
+        if ($buffer === false || strlen($buffer) < 5 || $buffer[0] !== $command[2]) {
             return null;
         }
 
@@ -186,4 +187,5 @@ class Query{
 
         return $redata;
     }
+
 }
