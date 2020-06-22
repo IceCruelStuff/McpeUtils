4a9012adea65729c1ba510d19a7f65bb32eca59b
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-21 17:29:37 -0700

    Update Rcon.php

--- src/mcpe/Rcon.php
+++ src/mcpe/Rcon.php
@@ -12,14 +12,15 @@ use function stream_set_timeout;
 use function strlen;
 use function unpack;
 
-class Rcon{
-    public const PACKET_COMMAND        = 2;
-    public const PACKET_LOGIN          = 3;
-    public const PACKET_LOGGER         = 4;
+class Rcon {
+
+    public const PACKET_COMMAND = 2;
+    public const PACKET_LOGIN = 3;
+    public const PACKET_LOGGER = 4;
     public const PACKET_PROTOCOL_CHECK = 9;
 
     public const RESPONSE_COMMAND = 0;
-    public const RESPONSE_LOGIN   = 2;
+    public const RESPONSE_LOGIN = 2;
 
     /** @var string */
     protected $host;
@@ -45,15 +46,15 @@ class Rcon{
     /** @var string */
     protected $errstr = "";
 
-    public function __construct(string $host, int $port, int $timeout = 10){
+    public function __construct($host, $port, $timeout = 10) {
         $this->host = $host;
         $this->port = $port;
         $this->timeout = $timeout;
     }
 
-    public function connect() : void{
+    public function connect() : void {
         $socket = @fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout);
-        if($socket === false || $errno){
+        if ($socket === false || $errno) {
             $this->connected = false;
 
             $this->errno = $errno;
@@ -68,26 +69,26 @@ class Rcon{
         $this->connected = true;
     }
 
-    public function isConnected() : bool{
+    public function isConnected() : bool {
         return $this->connected;
     }
 
-    private function disconnect() : void{
+    private function disconnect() : void {
         fclose($this->socket);
     }
 
-    public function authorize(string $password) : bool{
-        if(!$this->isConnected()){
+    public function authorize(string $password) : bool {
+        if (!$this->isConnected()) {
             return false;
         }
 
         $this->writePacket(++$this->currentRequestId, self::PACKET_LOGIN, $password);
         $response_packet = $this->readPacket();
-        if(
-            $response_packet !== null and
-            $response_packet['requestid'] === $this->currentRequestId and
+        if (
+            $response_packet !== null &&
+            $response_packet['requestid'] === $this->currentRequestId &&
             $response_packet['response'] === self::RESPONSE_LOGIN //TODO: This is check is useless, because server sends this as 2 every time
-        ){
+        ) {
             $this->authorized = true;
             return true;
         }
@@ -98,26 +99,26 @@ class Rcon{
         return false;
     }
 
-    public function sendCommand(string $command) : ?string{
-        if(!$this->isConnected()){
+    public function sendCommand(string $command) : ?string {
+        if (!$this->isConnected()) {
             return null;
         }
 
         $this->writePacket(++$this->currentRequestId, self::PACKET_COMMAND, $command);
         $response_packet = $this->readPacket();
-        if(
-            $response_packet !== null and
-            $response_packet['requestid'] === $this->currentRequestId and
+        if (
+            $response_packet !== null &&
+            $response_packet['requestid'] === $this->currentRequestId &&
             $response_packet['response'] === self::RESPONSE_COMMAND
-        ){
+        ) {
             return $this->lastResponse = $response_packet['payload'];
         }
 
         return null;
     }
 
-    private function writePacket(int $requestID, int $packetType, string $packetBody) : void{
-        if(!$this->isConnected()){
+    private function writePacket($requestID, $packetType, $packetBody) : void {
+        if (!$this->isConnected()) {
             return;
         }
 
@@ -130,23 +131,24 @@ class Rcon{
      * @return int|string[]
      * @phpstan-return array{requestid: int, response: int, payload: string}
      */
-    private function readPacket() : ?array{
-        if(!$this->isConnected()){
+    private function readPacket() : ?array {
+        if (!$this->isConnected()) {
             return null;
         }
 
         $size_packed = fread($this->socket, 4);
-        if($size_packed === false){
+        if ($size_packed === false) {
             return null;
         }
 
         $size = unpack("V", $size_packed)[1];
 
         $packet_serialized = fread($this->socket, $size);
-        if($packet_serialized === false){
+        if ($packet_serialized === false) {
             return null;
         }
 
         return unpack('V1requestid/V1response/a*payload', $packet_serialized);
     }
+
 }
